16b9d99b77f7afadcfe6e2eae47a7536cc806a66
Maycon Bruno Montes Gomes Constancio <MayconBruno@smcebr.net>
2024-01-15 17:11:51 -0300

    Finalize

--- Chat.py
+++ Chat.py
@@ -19,11 +19,51 @@ def main(pagina):
 
     nome_usuario = ft.TextField(label="Escreva seu nome")
 
+    chat = ft.Column()
+    
+    def enviar_mensagem_tunel(informacoes):
+        chat.controls.append(ft.Text(informacoes))
+        pagina.update()
+
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+
+    def enviar_mensagem(evento):
+        # colocar o nome do usuário nas mensagens
+        texto_campo_mensagem = f"{nome_usuario.value}: {campo_mensagem.value}"  
+        
+
+        pagina.pubsub.send_all(texto_campo_mensagem)
+        # limpar o campo_mensagem
+        campo_mensagem.value = ""
+        pagina.update()
+
+    campo_mensagem = ft.TextField(label="Escreva sua mensagem aqui", on_submit=enviar_mensagem)
+
+    botao_enviar = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+
+    def entrar_chat(evento):
+        # feche o popup
+        popup.open=False
+        # tirar o botão "iniciar chat" da tela
+        pagina.remove(botao_iniciar)
+        # adicionar o chat
+        pagina.add(chat)
+        # criar campo de enviar mensagem
+        linha_mensagem = ft.Row(
+            [campo_mensagem, botao_enviar]
+        )
+        pagina.add(linha_mensagem)
+        # botão de enviar mensagem
+        texto = f"{nome_usuario.value} entrou no chat"
+        pagina.pubsub.send_all(texto)
+        pagina.update()
+
     popup = ft.AlertDialog(
         open=False, 
         modal=True, 
         title=ft.Text("Bem-vindo ao chatName"),
-        content=nome_usuario
+        content=nome_usuario,
+        actions=[ft.ElevatedButton("Entrar", on_click=entrar_chat)]
         )
 
     def iniciar_chat(eventoClique):
@@ -36,6 +76,6 @@ def main(pagina):
     pagina.add(titulo)
     pagina.add(botao_iniciar)
 
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER)
 
     
